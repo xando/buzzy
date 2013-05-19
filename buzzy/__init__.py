ea3fc7962f554e866f9e28a0dbccb693e707c45d
xando <sebastian.pawlus@gmail.com>
2013-05-19 23:29:32 +0200

    commands and arguments handling

--- buzzy/__init__.py
+++ buzzy/__init__.py
@@ -1,11 +1,11 @@
 import os
 import sys
 import time
+import args
 import codecs
 import hashlib
 import inspect
 import fnmatch
-import argparse
 import SimpleHTTPServer
 import SocketServer
 
@@ -27,7 +27,9 @@ def get_class():
 
 
 class memoized(object):
+
     def __init__(self, func):
+        self.register.append(self)
         self.func = func
         self.cache = {}
 
@@ -61,6 +63,20 @@ class render(object):
         return wrapped_f
 
 
+class command(object):
+    register = []
+
+    def __init__(self, func):
+        self.func = func
+        self.register.append(func.func_name)
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __get__(self, obj, objtype):
+        return partial(self.__call__, obj)
+
+
 class Base(object):
 
     _register = []
@@ -75,19 +91,17 @@ class Base(object):
     read = lambda self, n: read(n)
     write = lambda self, n,c: write(n, c)
 
-    def __init__(self, *args, **kwargs):
-        super(Base, self).__init__(*args, **kwargs)
+    def _build(self):
         atributes = [getattr(self,a) for a in dir(self)]
 
         self.renderers = [
             func() for func in atributes if hasattr(func, 'render')
         ]
+
         self.renderers.extend([
             func(self) for func in atributes if isinstance(func, render)
         ])
 
-
-    def build(self):
         self.BUILD_DIR = path(self.BUILD_DIR)
         if self.BUILD_DIR.exists:
             [element.rm(r=True) for element in self.BUILD_DIR]
@@ -101,17 +115,17 @@ class Base(object):
 
         render_objects = []
         for element in filter(lambda x:x, [r for r in self.renderers]):
-            if type(element) in [list, tuple]:
+            if type(element[0]) in [list, tuple]:
                 render_objects.extend(element)
             else:
                 render_objects.append(element)
 
-        for element in render_objects:
-            directory = path(element['name']).dir()
+        for name, content in render_objects:
+            directory = path(name).dir()
             directory_build = self.BUILD_DIR / directory
             if directory and not directory_build.exists:
                 directory_build.mkdir(p=True)
-            self.write(self.BUILD_DIR / element['name'], element['content'])
+            self.write(self.BUILD_DIR / name, content)
 
         print "Generated %s" % datetime.now()
 
@@ -127,7 +141,7 @@ class Base(object):
             current_hash =  hashlib.md5("".join(hash_elements)).hexdigest()
             if current_hash != old_hash:
                 old_hash = current_hash
-                self.build()
+                self._build()
             time.sleep(0.2)
 
     def _server(self):
@@ -140,54 +154,45 @@ class Base(object):
         print "serving at port %s" % self.SERVER_PORT
         httpd.serve_forever()
 
-    def server(self):
-        watch = Process(target=self._watch)
-        watch.start()
+    @command
+    def build(self, args):
+        self._build()
+
+    @command
+    def watch(self, args):
+        self._watch()
+
+    @command
+    def server(self, args):
         server = Process(target=self._server)
         server.start()
 
         try:
-            watch.join()
-            server.join()
+            if '--no-watch' not in args.flags:
+                self._watch()
         except KeyboardInterrupt:
             server.terminate()
-            watch.terminate()
 
+    @command
+    def create(args):
+        blog_dir = (os.getcwd() / args.name).mkdir()
+        package_dir = path(__file__).dir()
 
-def server(args):
-    get_class().server()
-
-
-def build(args):
-    get_class().build()
-
-
-def create(args):
-    blog_dir = (os.getcwd() / args.name).mkdir()
-    package_dir = path(__file__).dir()
-
-    (package_dir / 'index.html').cp(blog_dir / 'index.html')
-    (package_dir / 'libs').cp(blog_dir / 'libs', r=True)
-    (package_dir / 'posts').cp(blog_dir / 'posts', r=True)
-    (package_dir / 'img').cp(blog_dir / 'img', r=True)
+        (package_dir / 'index.html').cp(blog_dir / 'index.html')
+        (package_dir / 'libs').cp(blog_dir / 'libs', r=True)
+        (package_dir / 'posts').cp(blog_dir / 'posts', r=True)
+        (package_dir / 'img').cp(blog_dir / 'img', r=True)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
+    klass = get_class()
 
-    parser_create = subparsers.add_parser('create')
-    parser_create.add_argument('name', type=str, help='name')
-    parser_create.set_defaults(func=create)
-
-    parser_build = subparsers.add_parser('build')
-    parser_build.set_defaults(func=build)
-
-    parser_server = subparsers.add_parser('server')
-    parser_server.set_defaults(func=server)
-
-    args = parser.parse_args()
-    args.func(args)
+    if args.all:
+        arg_0 = args.all[0]
+        if arg_0 not in command.register:
+            print "No such command '%s'" % arg_0
+        else:
+            getattr(klass, arg_0)(args)
 
 if __name__ == "__main__":
     main()
