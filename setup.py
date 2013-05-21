573bc8def59e7b84f884ffe3fc26eb3003bda60d
xando <sebastian.pawlus@gmail.com>
2013-05-21 23:12:03 +0200

    cleaning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'buzzy',
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'Static site generator',
     license = 'BSD',
     author = 'Sebastian Pawluś',
