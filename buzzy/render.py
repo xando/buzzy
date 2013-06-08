41f40444296c73de2ebe2f4015831114d6ab8e9f
xando <sebastian.pawlus@gmail.com>
2013-06-08 23:44:24 +0200

    Markdown renderer

--- buzzy/render.py
+++ buzzy/render.py
@@ -1,6 +1,8 @@
 from osome import path
 from jinja2 import Environment, FileSystemLoader
 
+from buzzy.packages import markdown as _markdown
+
 
 class render(object):
     pass
@@ -32,3 +34,14 @@ class template(content):
         env = Environment(loader=FileSystemLoader(self.klass.TEMPLATES_DIR))
         return env.get_template(path(template_name))
 
+
+class markdown(content):
+
+    def __init__(self, name, source):
+        md = _markdown.Markdown(extensions=[
+            'buzzy.packages.markdown.extensions.codehilite',
+            'buzzy.packages.markdown.extensions.meta'
+        ])
+        self.name = name
+        self.content = md.convert(path(source).content)
+        self.meta = {a:b if len(b) > 1 else b[0] for a,b in md.Meta.items()}
